68bae46aba7a7db54d4012f1d5b2db4365f830c1
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-09-03 12:26:06 -0400

    Add a Blazor UI Demo to the solution

---
+++ BlazorUI/Models/StudentDisplayModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Models
+{
+    public class StudentDisplayModel
+    {
+        [Required]
+        [Range(100000, 999999, ErrorMessage = "You need to enter a valid Student Id.")]
+        public int StudentId { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "You need to provide a long enough First Name.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "You need to provide a long enough Lastst Name.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
